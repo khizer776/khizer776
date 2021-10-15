8d3bf59cd5a5ebf8ccf0c286ca6b17dd5bd4f460
Khizer776 <khizerr776@gmail.com>
2021-10-15 19:01:33 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kjhgfd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
